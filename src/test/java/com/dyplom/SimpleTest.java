cce87b9f57c4cbfbb10174609e4918a4c3341671
Yevhenii Kuzmenko <zheniakuzma@gmail.com>
2021-09-24 17:25:00 +0300

    Added SimpleTest class

---
+++ src/test/java/com/dyplom/SimpleTest.java
@@ -0,0 +1,14 @@
+package com.dyplom;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class SimpleTest {
+
+    @Test
+    public void Should_Add_Group_To_Database() {
+        assertEquals(1,2) ;
+    }
+
+}
